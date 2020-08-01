75e8e3d26ec35e8c577c87a0a20182edff6626c8
Markus Kolb <makolb@n4v.eu>
2020-08-01 12:29:41 +0200

    chore(deps)!: update dependencies
    BREAKING CHANGE: minimum node version v10

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 "use strict";
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppError = void 0;
 var util = require("util");
 // instanceof is broken when class extends Error type
 // https://github.com/Microsoft/TypeScript/issues/13965
@@ -27,12 +38,12 @@ var AppError = /** @class */ (function (_super) {
         if (arg1 && typeof (arg1) === 'string') {
             message = arg1;
             if (params.length > 0) {
-                message = util.format.apply(util, [message].concat(params));
+                message = util.format.apply(util, __spreadArrays([message], params));
             }
         }
         else if (arg1 && arg1 instanceof Error) {
             if (params.length > 1) {
-                message = util.format.apply(util, [params[0]].concat((params.slice(1))));
+                message = util.format.apply(util, __spreadArrays([params[0]], (params.slice(1))));
             }
             else {
                 message = params[0];
@@ -63,7 +74,7 @@ var AppError = /** @class */ (function (_super) {
         _this.name = 'AppError';
         if (_this.message.startsWith('Error: ')) {
             if (params.length > 1) {
-                _this.message = util.format.apply(util, [params[0]].concat((params.slice(1)))) + ' (caused by: ' + _this.message + ')';
+                _this.message = util.format.apply(util, __spreadArrays([params[0]], (params.slice(1)))) + ' (caused by: ' + _this.message + ')';
             }
             else {
                 _this.message = params[0] + ' (caused by: ' + _this.message + ')';
